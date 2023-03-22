b2912c8a1a5a5bee276688287fd445cf3c554d54
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-03-22 14:20:55 +0600

    implement search funtionlaty

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,6 +7,7 @@ import Country from "./Country";
 const Body = () => {
   const countries = useCountries();
   const [filterCounties, setFilterCountries] = useState(null);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     setFilterCountries(countries);
@@ -19,18 +20,41 @@ const Body = () => {
     setFilterCountries(data);
   };
 
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setSearchText(value);
+    console.log(value);
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchText);
+    const searched = handleSearch(searchText);
+    setFilterCountries(searched);
+  };
+
+  const handleSearch = (query) => {
+    return countries.filter((country) =>
+      country?.name?.common.toLowerCase().includes(query)
+    );
+  };
+
   return (
     <div className="bg-light-gray min-h-[90vh] dark:bg-very-dark-blue text-very-dark-blue2">
       <div className="container mx-auto px-8 py-10">
         <section className="flex flex-col gap-8  md:flex-row md:items-center md:justify-between mb-2">
-          <div className="bg-white shadow flex px-6 py-4 md:w-1/3 gap-4 rounded-md dark:bg-dark-blue">
+          <form
+            onSubmit={handleOnSubmit}
+            className="bg-white shadow flex px-6 py-4 md:w-1/3 gap-4 rounded-md dark:bg-dark-blue"
+          >
             <Search className="text-dark-gray dark:text-white" />
             <input
+              onChange={handleChange}
+              value={searchText}
               className="text-dark-gray w-full dark:bg-dark-blue dark:text-white placeholder:dark:text-white outline-none"
               type="text"
               placeholder="Sarch for a country..."
             />
-          </div>
+          </form>
           <div className="bg-white shadow  rounded-md dark:bg-dark-blue p-4 md:w-1/6">
             <select
               className="cursor-pointer text-dark-gray dark:bg-dark-blue dark:text-white outline-none w-full"
@@ -49,11 +73,13 @@ const Body = () => {
             </select>
           </div>
         </section>
-        {
+        {filterCounties ? (
           <span className="text-very-dark-blue2  dark:text-light-gray font-semibold">
             Total Countires {filterCounties.length}
           </span>
-        }
+        ) : (
+          ""
+        )}
         <section className="py-8 grid gap-12 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {filterCounties ? (
             filterCounties.map((country) => (
